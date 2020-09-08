73d05205d678f9ceda91314015711237e47a342a
Ryan <ryanmitchell.dev@gmail.com>
2020-09-08 23:10:34 +0100

    Add project files.

---
+++ BoxShadowGenerator/Models/BoxShadowParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxShadowGenerator.Models
+{
+    public class BoxShadowParameters
+    {
+        public int XLength { get; set; }
+        public int YLength { get; set; }
+        public int BlurRadius { get; set; }
+        public int SpreadRadius { get; set; }
+        public int ShadowColour { get; set; }
+        public int BackgroundColour { get; set; }
+        public int BoxColour { get; set; }
+        public int Opacity { get; set; }
+    }
+}
---
+++ BoxShadowGenerator/Models/ShadowType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxShadowGenerator.Models
+{
+    public enum ShadowType
+    {
+        Outline,
+        Inset
+    }
+}
