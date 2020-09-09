354559917305e2847f20a40a666b007e9cc239b3
Ryan <ryanmitchell.dev@gmail.com>
2020-09-09 22:43:59 +0100

    implement colours

--- BoxShadowGenerator/Models/BoxShadowParameters.cs
+++ BoxShadowGenerator/Models/BoxShadowParameters.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -12,19 +14,27 @@ namespace BoxShadowGenerator.Models
         public int YLength { get; set; }
         public int BlurRadius { get; set; }
         public int SpreadRadius { get; set; }
-        public int ShadowColour { get; set; }
-        public int BackgroundColour { get; set; }
-        public int BoxColour { get; set; }
+        public string ShadowColour { get; set; } = "#000000";
+        public string BackgroundColour { get; set; } = "#ffffff";
+        public string BoxColour { get; set; } = "#000000";
         public float Opacity { get; set; } = 1;
-        public ShadowType Type { get; set; }
+        public ShadowType Type { get; set; } = ShadowType.Outline;
 
-        public string ParamString => $"{XLength}px {YLength}px {BlurRadius}px {SpreadRadius}px {RGBA()}";
+        public string ParamString => $"{XLength}px {YLength}px {BlurRadius}px {SpreadRadius}px {HexToColorString()}";
 
+        public string HexToColorString()
+        {         
+            string color = "";
+            if (ShadowColour.IndexOf('#') != -1)
+                color = ShadowColour.Replace("#", "");
 
-        private string RGBA()
-        {
-            var rgba = $"rgba(0,0,0,{Opacity})";
-            return rgba;
+            int r, g, b = 0;
+
+            r = int.Parse(color.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+            g = int.Parse(color.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+            b = int.Parse(color.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+
+            return string.Format("rgba({0}, {1}, {2}, {3});", r, g, b, Opacity);
         }
 
         public string ToCss()
--- BoxShadowGenerator/Program.cs
+++ BoxShadowGenerator/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace BoxShadowGenerator
 {
@@ -16,6 +19,14 @@ namespace BoxShadowGenerator
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services
+           .AddBlazorise(options =>
+           {
+               options.ChangeTextOnKeyPress = true;
+           })
+           .AddBootstrapProviders()
+           .AddFontAwesomeIcons();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
